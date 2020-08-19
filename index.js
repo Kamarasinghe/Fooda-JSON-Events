c0edecd5e28445a48f76182381397bde053aba3d
Kamarasinghe <kamarasinghe@nevada.unr.edu>
2020-08-19 13:32:42 -0700

    Implemented 'createReport' function along with helpers

--- index.js
+++ index.js
@@ -47,6 +47,47 @@ const rewardsReport = {
 
   parseTimeStamp: (timestamp) => {
     return parseInt(timestamp.substring(11, 13))
+  },
+
+  createReport: (customers) => {
+    let result = [];
+    let collectedCustomers = rewardsReport.collectCustomersIntoArray(customers);
+    let sortedCustomers = rewardsReport.sortByTotalRewardPoints(collectedCustomers);
+
+    sortedCustomers.forEach((customer, idx) => {
+      let pointOrPoints = rewardsReport.determineIfPointOrPoints(customer.totalRewardPoints);
+
+      if (customer.totalRewardPoints === 0) {
+        result.push(`${customer.name}: No orders.`);
+      } else if (idx === 0) {
+        result.push(`${customer.name}: ${customer.totalRewardPoints} ${pointOrPoints} with ${customer.avgPointsPerOrder} ${pointOrPoints} per order.`)
+      } else {
+        result.push(`${customer.name}: ${customer.totalRewardPoints} ${pointOrPoints} with ${customer.avgPointsPerOrder} per order.`)
+      }
+    });
+
+    return result.join('\n')
+  },
+  
+  determineIfPointOrPoints: (val) => {
+    return val > 1 ? 'points' : 'point';
+  },
+
+  collectCustomersIntoArray: (customers) => {
+    let names = Object.keys(customers);
+    let collectedCustomers = [];
+
+    names.forEach((name, idx) => {
+      collectedCustomers.push({ name, ...customers[name] });
+    });
+
+    return collectedCustomers;
+  },
+  
+  sortByTotalRewardPoints: (customers) => {
+    return customers.sort((a, b) => {
+      return b.totalRewardPoints - a.totalRewardPoints;
+    });
   }
 };
 
